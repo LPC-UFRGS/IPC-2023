f58c5850a8dd1e26adf26f42a2728b80a669b245
vlcanesin <vlcanesin@gmail.com>
2023-06-20 21:58:16 -0300

    Add 1001-extremamente-basico

---
+++ Semana 1/1001-extremamente-basico.py
@@ -0,0 +1,14 @@
+'''
+    Problema 1001 - Extremamente Básico
+    Víctor Lucas Rosada Canesin
+'''
+
+A = input()     # A e B são lidos como "string" 
+B = input()     # (sequência de caracteres, texto)
+
+A = int(A)      # Portanto, devem ser convertidos
+B = int(B)      # em inteiros
+
+X = A + B
+
+print(f"X = {X}")   #f"caracteres literais {variáveis}"
